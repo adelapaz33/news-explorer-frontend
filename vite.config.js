dba6415fb5d48ba5014a4567a5a349e863773b74
Alison de la Paz <arhibschle@gmail.com>
2025-08-28 18:01:51 -0400

    Route fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/news-explorer-frontend/",
+  base:
+    process.env.NODE_ENV === "production" ? "/news-explorer-frontend/" : "/",
   plugins: [react()],
   server: {
     port: 3003,
